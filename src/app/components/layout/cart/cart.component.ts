32ac4409a1c1f813de5c71fab577ce44b0b9b245
manhhd <hoangmanh1505@gmail.com>
2021-06-10 17:03:40 +0700

    update cart component

--- src/app/components/layout/cart/cart.component.ts
+++ src/app/components/layout/cart/cart.component.ts
@@ -1,35 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { Item } from 'src/app/shared/model/item';
 import { CartService } from 'src/app/service/cart.service';
+import { AppConfigService } from 'src/app/service/app-config.service';
 
 @Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css'],
+    selector: 'app-cart',
+    templateUrl: './cart.component.html',
+    styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
-  items: Item[];
-  totalAmount: number;
+    items: Item[];
+    totalAmount: number;
+    baseUrl: string;
 
-  constructor(private cartService: CartService) {
-  }
+    constructor(
+        private configService: AppConfigService,
+        private cartService: CartService,
+    ) {
+    }
 
-  ngOnInit(): void {
-    this.cartService.itemEvent.subscribe(e => {
-      this.items = e;
-    });
-    this.cartService.getItems();
-    this.totalAmount = this.cartService.getTotalAmount();
-  }
+    ngOnInit(): void {
+        this.baseUrl = this.configService.getConfig().api.baseUrl;
+        this.cartService.itemEvent.subscribe(e => {
+            this.items = e;
+        });
+        this.cartService.getItems();
+        this.getTotalAmount();
+    }
 
-  updateQuantity(itemId: number, value: number): void {
-    const index = this.items.findIndex(e => e.product.id === itemId);
-    this.items[index].quantity = value;
-  }
+    updateQuantity(itemId: number, value: number): void {
+        const index = this.items.findIndex(e => e.product.id === itemId);
+        this.items[index].quantity = value;
+        this.getTotalAmount();
+    }
 
-  removeItem(event, id): void {
-    event.preventDefault();
-    this.cartService.removeItem(id);
-    this.totalAmount = this.cartService.getTotalAmount();
-  }
+    removeItem(event, id): void {
+        event.preventDefault();
+        this.cartService.removeItem(id);
+        this.totalAmount = this.cartService.getTotalAmount();
+    }
+
+    getTotalAmount(): void {
+        this.totalAmount = this.items.map(({quantity, product}) => product.price * quantity).reduce((a, b) => a + b, 0);
+    }
 }
